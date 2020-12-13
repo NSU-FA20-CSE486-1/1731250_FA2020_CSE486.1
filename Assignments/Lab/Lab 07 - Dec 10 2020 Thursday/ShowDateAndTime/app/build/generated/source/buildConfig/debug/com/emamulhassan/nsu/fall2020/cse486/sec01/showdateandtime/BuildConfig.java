dfc0bd41cf415910b9867037a57a4480b9e31611
Emamul Hassan <emamul.hassan@northsouth.edu>
2020-12-13 18:21:57 +0600

    Enhancemet #43 Lab 7 in class coding Assignment
    Time and Date selection task.

---
+++ Assignments/Lab/Lab 07 - Dec 10 2020 Thursday/ShowDateAndTime/app/build/generated/source/buildConfig/debug/com/emamulhassan/nsu/fall2020/cse486/sec01/showdateandtime/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.emamulhassan.nsu.fall2020.cse486.sec01.showdateandtime;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.emamulhassan.nsu.fall2020.cse486.sec01.showdateandtime";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
