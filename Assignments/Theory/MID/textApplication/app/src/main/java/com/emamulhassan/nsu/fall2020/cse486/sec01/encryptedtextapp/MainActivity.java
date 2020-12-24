049163d8f2c255765da4aada0bc4f3dbd4cf33b5
Emamul Hassan <emamul.hassan@northsouth.edu>
2020-12-24 16:29:21 +0600

    Assignment #44: Creating Encrypt Methods
    Creating Encrypt Method for the application.

--- Assignments/Theory/MID/textApplication/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/encryptedtextapp/MainActivity.java
+++ Assignments/Theory/MID/textApplication/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/encryptedtextapp/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,7 +61,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private String encrypt(String toString, String toString1) {
+    private String encrypt(String encMsg, String encKey) throws Exception {
+
+        SecretKeySpec key = generateKey(encKey);
+        Cipher cipher = Cipher.getInstance(AES);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = cipher.doFinal(encMsg.getBytes());
+        String encryptedValue = Base64.encodeToString(encVal, Base64.DEFAULT);
+        return encryptedValue;
+
+    }
+
+    private SecretKeySpec generateKey(String encKey) throws Exception{
+
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = encKey.getBytes("UTF-8");
+        digest.update(bytes, 0, bytes.length);
+        byte[] key = digest.digest();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        return secretKeySpec;
+
 
     }
 }
