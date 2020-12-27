7722088c3cdb42a9c862259066d85f06bf8af57b
Emamul Hassan <emamul.hassan@northsouth.edu>
2020-12-27 16:44:49 +0600

    Enhancement #47: Fixed margintop and updated .java
    Fixed margintop and updated .java of main activity.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
@@ -3,12 +3,20 @@ package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton, loginButtonVendor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn_vendor);
     }
 }
