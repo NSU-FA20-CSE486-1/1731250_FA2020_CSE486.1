d0332f0d6150444b21f941527ea6452c3510390b
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-04 19:06:17 +0600

    Enhancement #46: Creating two new packages
    Creating prevalent and users class to varify a users login credentials from firebase database.

---
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Prevalent;
+
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
