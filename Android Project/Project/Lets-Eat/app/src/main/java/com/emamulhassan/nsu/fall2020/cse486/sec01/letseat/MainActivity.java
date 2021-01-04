97c66bbf0e279af9446b75dddea824b669856ea2
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-04 20:01:29 +0600

    Enhancement #48: setOnClickListener for login buttons
    Initialized setOnClickListener for login buttons for both vendor and customer for redirect to respected actiivty.[same code was used in my cse299.3 final project]

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
@@ -1,13 +1,31 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Users;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNowButton, loginButton, loginButtonVendor;
+    private ProgressDialog loadingBar;
+
 
 
     @Override
@@ -17,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
-        loginButton = (Button) findViewById(R.id.main_login_btn_vendor);
+        loginButtonVendor = (Button) findViewById(R.id.main_login_btn_vendor);
+
+        Paper.init(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButtonVendor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        
     }
 }
