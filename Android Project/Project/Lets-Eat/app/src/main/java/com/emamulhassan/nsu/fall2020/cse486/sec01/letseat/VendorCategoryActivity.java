974048d60926098e7ef85949c6ecf995dc9f3db5
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-04 22:32:35 +0600

    Enhancement #52: Initializing All Imageviews
    Initialized all imageview of each category from VendorCategory.xml

---
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorCategoryActivity.java
@@ -0,0 +1,35 @@
+package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class VendorCategoryActivity extends AppCompatActivity {
+
+    private ImageView BreakfastItem;
+    private ImageView LunchItem;
+    private ImageView DessertItem;
+    private ImageView DrinksItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_category);
+
+        BreakfastItem = (ImageView) findViewById(R.id.breakfast);
+
+        LunchItem = (ImageView) findViewById(R.id.lunch);
+
+        DessertItem = (ImageView) findViewById(R.id.dessert);
+
+        DrinksItem = (ImageView) findViewById(R.id.drinks);
+
+
+
+
+
+    }
+}
