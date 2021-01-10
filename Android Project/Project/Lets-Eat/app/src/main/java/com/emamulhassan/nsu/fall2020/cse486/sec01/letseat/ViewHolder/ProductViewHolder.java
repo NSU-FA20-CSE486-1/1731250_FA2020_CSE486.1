f105203e6bf2452011ce5552b753db1cd3698eab
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-10 15:14:13 +0600

    Enhancement #56: Updated ProductViewHolder.java
    ProductViewHolder.java of ViewHolder Package.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ViewHolder/ProductViewHolder.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ViewHolder/ProductViewHolder.java
@@ -1,4 +1,40 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ViewHolder;
 
-public class ProductViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Interface.ItemClickListner;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public ImageView imageView;
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ItemClickListner listner;
+
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_description);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view, getAdapterPosition(), false);
+    }
 }
