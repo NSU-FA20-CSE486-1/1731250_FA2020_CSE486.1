cccd49740d0a87cb8a1b67c9a41b199bda007daf
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-10 14:47:57 +0600

    Enhancement #54: Created Navigation Drawer Activity
    Created Navigation Drawer Activity for customer homepage.

---
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
