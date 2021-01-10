b738171c6da3ded87865bd4b3a3232987f7b61d4
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-10 15:08:11 +0600

    Enhancement #56: Created Interface package
    Created ItemClickListner in Interface package.

---
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/Interface/ItemClickListner.java
@@ -0,0 +1,8 @@
+package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
