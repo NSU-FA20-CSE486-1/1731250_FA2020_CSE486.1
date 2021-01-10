77b92fca70a81bdf3d36418ebaf74691bfdbe18d
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-10 14:55:42 +0600

    Enhancemenet #56: Added String types
    Added String types of Product details.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/Model/Products.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/Model/Products.java
@@ -2,7 +2,12 @@ package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model;
 
 public class Products
 {
-    
+    private String pname, description, price, image, category, pid, date, time;
+
+    public Products()
+    {
+
+    }
 
 
 }
