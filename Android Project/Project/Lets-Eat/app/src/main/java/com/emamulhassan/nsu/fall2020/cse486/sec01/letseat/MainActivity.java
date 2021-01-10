3c15ab87ac5846ae6c92c10d4cb93e769ca57c57
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-10 17:03:17 +0600

    Enhancement #54: Added login support for Customer
    Added login support for Customer in mainactivity.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/MainActivity.java
@@ -103,9 +103,9 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "Please wait, you are already logged in...", Toast.LENGTH_SHORT).show();
                             loadingBar.dismiss();
 
-//                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-//                            Prevalent.currentOnlineUser = usersData;
-//                            startActivity(intent);
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            Prevalent.currentOnlineUser = usersData;
+                            startActivity(intent);
                         }
                         else
                         {
