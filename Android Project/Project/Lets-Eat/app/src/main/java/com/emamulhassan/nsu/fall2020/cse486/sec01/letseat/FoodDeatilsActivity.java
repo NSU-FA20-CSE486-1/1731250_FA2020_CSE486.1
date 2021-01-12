1c857a1a1712a986b5fc31f36f137671470aa1b6
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-12 23:29:25 +0600

    Enhancement #57: Used HashMap to retreive details
    Used HashMap to retreive details  from firebase database.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/FoodDeatilsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/FoodDeatilsActivity.java
@@ -1,14 +1,120 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Products;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class FoodDeatilsActivity extends AppCompatActivity {
+
+    private ImageView productImage;
+    private TextView productName, productDescription, productPrice;
+    private ElegantNumberButton numberButton;
+    private Button addToCartButton;
+    private String productID = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_deatils);
+
+        productImage = (ImageView) findViewById(R.id.product_image_details);
+        productName = (TextView) findViewById(R.id.product_name_details);
+        productDescription = (TextView) findViewById(R.id.product_description_details);
+        productPrice = (TextView) findViewById(R.id.product_price_details);
+        numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
+        addToCartButton = (Button) findViewById(R.id.pd_add_to_cart_button);
+
+        productID = getIntent().getStringExtra("pid");
+
+
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                addingToCartList();
+            }
+        });
+
     }
+
+    private void addingToCartList()
+    {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd MMM, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("pid", productID);
+        cartMap.put("pname", productName.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberButton.getNumber());
+        cartMap.put("discount", "");
+
+        cartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone())
+                .child("Products").child(productID)
+                .updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        cartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone())
+                                .child("Products").child(productID)
+                                .updateChildren(cartMap)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task)
+                                    {
+                                        if (task.isSuccessful())
+                                        {
+                                            Toast.makeText(FoodDeatilsActivity.this, "Added to Cart List.", Toast.LENGTH_SHORT).show();
+
+                                            Intent intent = new Intent(FoodDeatilsActivity.this, HomeActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+                                });
+                    }
+                });
+
+
+
+    }
+
+
+
+
 }
