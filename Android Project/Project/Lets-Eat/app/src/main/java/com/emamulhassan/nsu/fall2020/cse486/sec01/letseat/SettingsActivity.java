892c92be60484bd3536ee74e32a3c0a56cb89192
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-12 23:45:34 +0600

    Enhancement #58: Initialized all variables and setOnClickListener for 2 buttons
    Initialized all variables and setOnClickListener for 2 buttons close and update.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SettingsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SettingsActivity.java
@@ -2,13 +2,68 @@ package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class SettingsActivity extends AppCompatActivity{
+
+    private TextView closeTextBtn, saveTextButton;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText;
+    private String checker = "";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        closeTextBtn = (TextView) findViewById(R.id.close_settings_btn);
+        saveTextButton = (TextView) findViewById(R.id.update_account_settings_btn);
+        fullNameEditText = (EditText) findViewById(R.id.settings_full_name);
+        userPhoneEditText = (EditText) findViewById(R.id.settings_phone_number);
+        addressEditText = (EditText) findViewById(R.id.settings_address);
+
+
+
+
+        userInfoDisplay(fullNameEditText, userPhoneEditText, addressEditText);
+
+
+        closeTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+
+
+        saveTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                updateOnlyUserInfo();
+            }
+        });
+
     }
+
+
+
+    
 }
