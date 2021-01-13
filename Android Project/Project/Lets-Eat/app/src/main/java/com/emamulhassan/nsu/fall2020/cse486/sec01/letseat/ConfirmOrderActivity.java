08f7a1e09d8827e7a5d4fbd9b7d35cc7da8e1eb9
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-13 19:29:18 +0600

    Enhancement #60: Initialized all ids of activity_confirm_order.xml
    Initialized all ids of activity_confirm_order.xml and showing a toast message of Total price.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ConfirmOrderActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ConfirmOrderActivity.java
@@ -3,12 +3,32 @@ package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
+
+    private EditText nameEditText, phoneEditText, addressEditText, cityEditText;
+    private Button confirmOrderBtn;
+    private String totalAmount = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
+
+        totalAmount = getIntent().getStringExtra("Total Price");
+        Toast.makeText(this, "Total Price = " + totalAmount + " BDT", Toast.LENGTH_SHORT).show();
+
+        confirmOrderBtn = (Button) findViewById(R.id.confirm_final_order_btn);
+        nameEditText = (EditText) findViewById(R.id.shippment_name);
+        phoneEditText = (EditText) findViewById(R.id.shippment_phone_number);
+        addressEditText = (EditText) findViewById(R.id.shippment_address);
+        cityEditText = (EditText) findViewById(R.id.shippment_city);
+
+
     }
 }
