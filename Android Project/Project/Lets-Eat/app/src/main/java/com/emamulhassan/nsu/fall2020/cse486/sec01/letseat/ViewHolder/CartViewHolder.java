c455f3a6cf34a858d1abe21151e97d4a1a4fad46
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-13 18:52:56 +0600

    Enhancement #56: Updated CartViewHolder.java
    Updated CartViewHolder.java[same code was used in my cse299.3 final project]

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ViewHolder/CartViewHolder.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/ViewHolder/CartViewHolder.java
@@ -1,4 +1,36 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ViewHolder;
 
-public class CartViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Interface.ItemClickListner;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductPrice, txtProductQuantity;
+    private ItemClickListner itemClickListner;
+
+    public CartViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        itemClickListner.onClick(view, getAdapterPosition(), false);
+    }
+
+    public void setItemClickListner(ItemClickListner itemClickListner)
+    {
+        this.itemClickListner = itemClickListner;
+    }
 }
