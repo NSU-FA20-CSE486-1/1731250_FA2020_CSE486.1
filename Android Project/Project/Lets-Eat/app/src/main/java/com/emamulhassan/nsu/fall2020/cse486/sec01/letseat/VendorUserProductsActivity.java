11e002def770129554a73d3f566feb0c0e3727a1
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-17 20:32:45 +0600

    Enhancement #62: Used FirebaseRecyclerOptions and FirebaseRecyclerAdapte in VendorUserProductsActivity.java
    Updated VendorUserProductsActivity.java and used FirebaseRecyclerOptions and FirebaseRecyclerAdapter to view each order details.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorUserProductsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorUserProductsActivity.java
@@ -1,14 +1,95 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class VendorUserProductsActivity extends AppCompatActivity {
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Cart;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ViewHolder.CartViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class VendorUserProductsActivity extends AppCompatActivity
+{
+    private RecyclerView productsList;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference cartListRef;
+    private ImageView BackBtnOrder2;
+
+    private String userID = "";
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_user_products);
+
+
+        userID = getIntent().getStringExtra("uid");
+
+        BackBtnOrder2 = (ImageView) findViewById(R.id.back_btn_order2);
+        productsList = findViewById(R.id.products_list);
+        productsList.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        productsList.setLayoutManager(layoutManager);
+
+        BackBtnOrder2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VendorUserProductsActivity.this, VendorNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        cartListRef = FirebaseDatabase.getInstance().getReference()
+                .child("Cart List").child("Admin View").child(userID).child("Products");
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+
+        FirebaseRecyclerOptions<Cart> options =
+                new FirebaseRecyclerOptions.Builder<Cart>()
+                        .setQuery(cartListRef, Cart.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull Cart model)
+            {
+                holder.txtProductQuantity.setText("Quantity= " + model.getQuantity());
+                holder.txtProductPrice.setText("Price: " + model.getPrice() + " BDT");
+                holder.txtProductName.setText(model.getPname());
+            }
+
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items_layout, parent, false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+            }
+        };
+
+        productsList.setAdapter(adapter);
+        adapter.startListening();
     }
 }
