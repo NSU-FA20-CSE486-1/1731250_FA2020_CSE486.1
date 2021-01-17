72b4cb20541e260bdd836906f552e7292445c4bb
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-17 19:16:47 +0600

    Enhancement #61: Initialized all ids and connected orders_list
    Initialized all ids and connected orders_list to VendorNewOrdersActivity by using LinearLayoutManager

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorNewOrdersActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorNewOrdersActivity.java
@@ -1,14 +1,29 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class VendorNewOrdersActivity extends AppCompatActivity {
+
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_new_orders);
+
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+        ordersList = findViewById(R.id.orders_list);
+        ordersList.setLayoutManager(new LinearLayoutManager(this));
     }
+    
+
 }
