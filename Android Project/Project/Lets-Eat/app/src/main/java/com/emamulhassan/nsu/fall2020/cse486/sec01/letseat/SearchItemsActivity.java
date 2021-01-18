4c74afd65855b633967a156a242fa00920c9f3ab
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-18 13:29:39 +0600

    Enhancement #63: Added FirebaseRecyclerAdapter for Products, ProductViewHolder
    Added FirebaseRecyclerAdapter for Products, ProductViewHolder.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
@@ -1,20 +1,26 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Products;
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class SearchItemsActivity extends AppCompatActivity {
 
@@ -55,15 +61,50 @@ public class SearchItemsActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart()
+    {
         super.onStart();
+
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
 
         FirebaseRecyclerOptions<Products> options =
                 new FirebaseRecyclerOptions.Builder<Products>()
-                        .setQuery(reference.orderByChild("description").startAt(SearchInput), Products.class)
+                        .setQuery(reference.orderByChild("pname").startAt(SearchInput), Products.class)
                         .build();
 
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model)
+                    {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price: " + model.getPrice() + "BDT");
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                Intent intent = new Intent(SearchItemsActivity.this, FoodDeatilsActivity.class);
+                                intent.putExtra("pid", model.getPid());
+                                startActivity(intent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                    {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+
+        searchList.setAdapter(adapter);
+        adapter.startListening();
     }
 }
