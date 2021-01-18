fb789e1a555d77021d2a2082d3b2afb934df2085
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-18 12:51:06 +0600

    Enhancement #63: DatabaseReference for Search option
    DatabaseReference for Search option and addded search by description order to get the best out of searched item.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
@@ -11,12 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.emamulhassan.nsu.fall2020.cse486.sec01.letseat.Model.Products;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SearchItemsActivity extends AppCompatActivity {
 
     private ImageView SearchBtn;
     private Button CancelBtn;
     private EditText inputText;
     private RecyclerView searchList;
+    private String SearchInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,28 @@ public class SearchItemsActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        SearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SearchInput = inputText.getText().toString();
+
+                onStart();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        FirebaseRecyclerOptions<Products> options =
+                new FirebaseRecyclerOptions.Builder<Products>()
+                        .setQuery(reference.orderByChild("description").startAt(SearchInput), Products.class)
+                        .build();
 
     }
 }
