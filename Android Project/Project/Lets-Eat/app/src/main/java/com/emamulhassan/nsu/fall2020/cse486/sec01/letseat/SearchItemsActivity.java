46d504f3a2f4ca1d15a88fb810ab82fbeb8c7f98
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-18 12:42:29 +0600

    Enhancement #63: Initialized all ids and setOnClickListener for CancelBtn
    Initialized all ids and added setOnClickListener for CancelBtn.

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/SearchItemsActivity.java
@@ -1,14 +1,40 @@
 package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SearchItemsActivity extends AppCompatActivity {
+
+    private ImageView SearchBtn;
+    private Button CancelBtn;
+    private EditText inputText;
+    private RecyclerView searchList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_items);
+
+        inputText = findViewById(R.id.search_product_name);
+        SearchBtn = (ImageView) findViewById(R.id.search_btn);
+        CancelBtn = (Button) findViewById(R.id.cancel_btn);
+        searchList = findViewById(R.id.search_list);
+        searchList.setLayoutManager(new LinearLayoutManager(SearchItemsActivity.this));
+
+        CancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
