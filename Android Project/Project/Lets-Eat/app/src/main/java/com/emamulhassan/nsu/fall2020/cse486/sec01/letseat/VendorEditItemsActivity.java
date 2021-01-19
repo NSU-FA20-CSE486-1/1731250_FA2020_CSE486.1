b836fa4315c9d019fae3305349b27fcc826b64d5
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-19 18:53:15 +0600

    Enhancement #64: Initialized all ids and fixed an error in .xml file
    Initialized all ids and fixed an error in .xml file of VendorEditItemsActivity

--- Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorEditItemsActivity.java
+++ Android Project/Project/Lets-Eat/app/src/main/java/com/emamulhassan/nsu/fall2020/cse486/sec01/letseat/VendorEditItemsActivity.java
@@ -3,12 +3,26 @@ package com.emamulhassan.nsu.fall2020.cse486.sec01.letseat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class VendorEditItemsActivity extends AppCompatActivity {
+public class VendorEditItemsActivity extends AppCompatActivity
+{
+    private Button applyChangesBtn, deleteBtn;
+    private EditText name, price, description;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_edit_items);
+
+        applyChangesBtn = findViewById(R.id.apply_changes_btn);
+        name = findViewById(R.id.product_name_maintain);
+        price = findViewById(R.id.product_price_maintain);
+        description = findViewById(R.id.product_description_maintain);
+        imageView = findViewById(R.id.product_image_maintain);
+        deleteBtn = findViewById(R.id.delete_product_btn);
     }
 }
